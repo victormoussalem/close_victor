3b955c7f1c833c44d9c997c09ccfd17acf4d39ba
Victor <victor.moussalem@gmail.com>
2014-06-23 11:09:15 -0700

    added DB boilerplate

--- app/__init__.py
+++ app/__init__.py
@@ -10,8 +10,10 @@ app.secret_key = 'a2isj913ndf9*10f0f[if9]9un!nn%n'
 HEROKU = 'HEROKU' in os.environ
 
 if HEROKU:
+    print "heroku"
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 else:
+    print "not heroku"
     app.config['DEBUG'] = True
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://victor:password@localhost/close_projects'
     app.config['SQLALCHEMY_ECHO'] = False
