eef0a76f2078330b66d7449e6dfcf6acb9e71b3a
Victor <victor.moussalem@gmail.com>
2014-06-23 16:37:46 -0700

    heroku hack

--- app/__init__.py
+++ app/__init__.py
@@ -11,9 +11,9 @@ HEROKU = 'HEROKU' in os.environ
 
 if HEROKU:
     print "heroku"
-    print DATABASE_URL
-    print SQLALCHEMY_DATABASE_URI
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+#    print os.environ['DATABASE_URL']
+#    print os.environ['SQLALCHEMY_DATABASE_URI']
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://tifqluyrvevtah:w1Gz_CoDWcR9IniZPo0z_a1tjr@ec2-23-21-185-168.compute-1.amazonaws.com:5432/d129aq9pvnh9f'
 else:
     print "not heroku"
     app.config['DEBUG'] = True
